60c5a21c9020bdb15b75d66c4192b73fe73f0d1c
Pfftz <Kalixpfft+github@protonmail.com>
2025-03-21 20:16:25 +0700

    Add player and monster scripts, update level and project settings

--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+@onready var animation_tree = get_node("AnimationTree")
+@onready var playback = animation_tree.get("parameters/playback")
 @onready var player_mesh = get_node("Knight")
 
 @export var gravity: float = 9.8
@@ -8,12 +10,12 @@ extends CharacterBody3D
 @export var run_speed: float = 9.0
 
 #animation nodes names
-var idle_anim: String = "idle"
-var walk_anim: String = "walk"
-var run_anim: String = "run"
-var jump_anim: String = "jump"
-var attack1_anim: String = "attack1"
-var death_anim: String = "death"
+var idle_anim: String = "Idle"
+var walk_anim: String = "Walk"
+var run_anim: String = "Run"
+var jump_anim: String = "Jump"
+var attack1_anim: String = "Attack1"
+var death_anim: String = "Death"
 
 #state Machine Conditions
 var is_attacking: bool
@@ -42,7 +44,7 @@ func _ready():
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		aim_turn = -event.relative.x * 0.015
+		aim_turn = - event.relative.x * 0.015
 	if event.is_action_pressed("aim"):
 		direction = camroot_h.global_transform.basis.z.normalized()
 		# Store the desired rotation while aiming
@@ -51,6 +53,7 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta):
 	var on_floor = is_on_floor()
 	if !is_dying:
+		attack1()
 		if !on_floor:
 			vertical_velocity += Vector3.DOWN * gravity * 2 * delta
 		else:
@@ -61,6 +64,11 @@ func _physics_process(delta):
 		angular_acceleration = 10
 		movement_speed = 0
 		acceleration = 15
+
+		if (attack1_anim in playback.get_current_node()):
+			is_attacking = true
+		else:
+			is_attacking = false
 
 		var h_rot = camroot_h.global_transform.basis.get_euler().y
 		if (Input.is_action_pressed("forward") || Input.is_action_pressed("backward") || Input.is_action_pressed("left") || Input.is_action_pressed("right")):
@@ -90,3 +98,16 @@ func _physics_process(delta):
 		velocity.x = horizontal_velocity.x + vertical_velocity.x
 		velocity.y = vertical_velocity.y
 		move_and_slide()
+	animation_tree["parameters/conditions/IsOnFloor"] = on_floor
+	animation_tree["parameters/conditions/IsInAir"] = !on_floor
+	animation_tree["parameters/conditions/IsWalking"] = is_walking
+	animation_tree["parameters/conditions/IsRunning"] = is_running
+	animation_tree["parameters/conditions/IsNotWalking"] = !is_walking
+	animation_tree["parameters/conditions/IsNotRunning"] = !is_running
+	animation_tree["parameters/conditions/is_dying"] = is_dying
+
+func attack1():
+	if (idle_anim in playback.get_current_node()) or (walk_anim in playback.get_current_node()):
+		if Input.is_action_just_pressed("attack"):
+			if !is_attacking:
+				playback.travel(attack1_anim)
